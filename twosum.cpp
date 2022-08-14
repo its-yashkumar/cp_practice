5855b125519d6ad3249afc8876e7231063dcdd2a
Yashkumar Jain <jainy6214@gmail.com>
2022-08-14 14:47:43 +0530

    sorted the array

--- twosum.cpp
+++ twosum.cpp
@@ -4,8 +4,8 @@ int main(){
     // cout<<"hello world";
     vector<int> a={2,4,3,1,7,11};
     int size=a.size();
-    cout<<size;
-    
+    cout<<"The size of vector is :"<<size<<endl;
+    sort(v.begin(),v.end());
 
 
 }
