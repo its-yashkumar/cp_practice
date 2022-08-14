5fb9d74a69c0b02d8188885bc0fc07ed3b6ff9a0
Yashkumar Jain <jainy6214@gmail.com>
2022-08-14 14:54:47 +0530

    Two sum solved

--- twosum.cpp
+++ twosum.cpp
@@ -3,11 +3,11 @@ using namespace std;
 int main(){
     // cout<<"hello world";
     vector<int> a={2,4,3,1,7,11};
-    int target=5;
+    int target=6;
     int size=a.size();
     cout<<"The size of vector is :"<<size<<endl;
     int s=0,e=a.size()-1;
-    sort(v.begin(),v.end());
+    sort(a.begin(),a.end());
     while (s<e)
     {
         int sum=a[s]+a[e];
@@ -15,6 +15,14 @@ int main(){
             cout<<"Yes possible and elements at :"<<s<<" and "<<e<<endl;
             break;
         }
+        else if (sum>target)
+        {
+            e--;
+        }
+        else{
+            s++;
+        }
+        
 
     }
     
