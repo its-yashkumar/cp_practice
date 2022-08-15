c9948a2a140f78d86ee18496d56ff9764559f03a
Yashkumar Jain <jainy6214@gmail.com>
2022-08-15 12:40:23 +0530

    problem solved majority elemnets

--- findmajorele.cpp
+++ findmajorele.cpp
@@ -1,7 +1,25 @@
-/* A majority element is a element that apprears in an array more than n/2 time
+/* A majority element is a element that apprears in an array more than n/2 times.
 n is size of array*/
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+  vector<int> v={1,2,3,1,2,1,1,1};
+  sort(v.begin(),v.end());
+  int count=1;
+  for (int i = 1; i < v.size(); i++)
+  {
+    if(v[i]==v[i-1]){
+       count++;
+    }
+    else{
+     count=1;
+    }
+    if (count>(v.size()/2))
+    {
+        cout<<"The majority element is : "<<v[i];
+    }
+    
+  }
+  
 
 }
