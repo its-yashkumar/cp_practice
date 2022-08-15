696a1bdc483bfecdde1e198260a3261fbfa33f26
Yashkumar Jain <jainy6214@gmail.com>
2022-08-15 10:58:05 +0530

    solved using my original approach

--- maxstockprofit.cpp
+++ maxstockprofit.cpp
@@ -2,4 +2,25 @@
 A transaction is one buy+sell and you are allowed to do only one transation.
 You have to maximize the profit*/
 // Try Greedy approach
- 
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> price={5,2,6,9,8,11};
+    int min=9999,max=0,minindex,maxindex;
+    for (int i = 0; i < price.size(); i++)
+    {
+        if(price[i]>max){
+            max=price[i];
+            maxindex=i;
+        }
+        if (price[i]<min)
+        {
+            min=price[i];
+            minindex=i;
+        }
+        
+    }
+    cout<<"Max and index "<<max<<" "<<maxindex<<endl;
+    cout<<"Min and index "<<min<<" "<<minindex<<endl;
+    cout<<"Maximum profit :"<<max-min<<endl;
+}
