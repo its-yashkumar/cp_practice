65ec30be65c2b17ff00ceb77b0668857a20b9bec
Yashkumar Jain <jainy6214@gmail.com>
2022-08-15 10:50:43 +0530

    written problem statement

---
+++ maxstockprofit.cpp
@@ -0,0 +1,5 @@
+/* You have stock market prices of several days in an array.
+A transaction is one buy+sell and you are allowed to do only one transation.
+You have to maximize the profit*/
+// Try Greedy approach
+ 
