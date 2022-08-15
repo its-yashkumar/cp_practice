d5b3ff7a353ee9b85b945382ee500e6141de4c25
Yashkumar Jain <jainy6214@gmail.com>
2022-08-15 11:14:16 +0530

    found optimal approach

--- maxstockprofit.cpp
+++ maxstockprofit.cpp
@@ -2,6 +2,10 @@
 A transaction is one buy+sell and you are allowed to do only one transation.
 You have to maximize the profit*/
 // Try Greedy approach
+
+/*                     Optimal Approach                      
+Find highest selling price and find the minimum buying price in past days
+*/
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
