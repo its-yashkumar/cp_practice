50a957c4073bd1d804120d4dda2657bcede62861
Yashkumar Jain <jainy6214@gmail.com>
2022-08-15 10:59:59 +0530

    there was a catch in that approach so discarded

--- maxstockprofit.cpp
+++ maxstockprofit.cpp
@@ -7,20 +7,5 @@ using namespace std;
 int main(){
     vector<int> price={5,2,6,9,8,11};
     int min=9999,max=0,minindex,maxindex;
-    for (int i = 0; i < price.size(); i++)
-    {
-        if(price[i]>max){
-            max=price[i];
-            maxindex=i;
-        }
-        if (price[i]<min)
-        {
-            min=price[i];
-            minindex=i;
-        }
-        
-    }
-    cout<<"Max and index "<<max<<" "<<maxindex<<endl;
-    cout<<"Min and index "<<min<<" "<<minindex<<endl;
-    cout<<"Maximum profit :"<<max-min<<endl;
+   
 }
