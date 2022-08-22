e1e1a34663aead85cad2775aa56e2c1a4a0ef5ce
Yashkumar Jain <jainy6214@gmail.com>
2022-08-22 20:19:40 +0530

    The map approach for enagram problem

--- stringenagram.cpp
+++ stringenagram.cpp
@@ -9,6 +9,37 @@ Given two strings find whether they are enagrams or not?
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    string s1="apple";
+   string s2="elapp";
+    map<char,int> count1;
+    for (int i = 0; i < s1.size(); i++)
+   {
+    count1[s1[i]]++;
+   }
+   for (int i = 0; i < s2.size(); i++)
+   {
+    count1[s2[i]]--;
+    //When using only count1 
+    //count1[s2[i]-'a']--;
+   }
+   int flag=1;
+   for (char i = 'a'; i <= 'z'; i++)
+   {
+     if(count1[i]!=0){
+        flag=0;
+        cout<<"An enagram";
+        break;
+     }
+   }
+   if(flag){
+    cout<<"yes its an anagram";
+   }
+   
+
+}
+/*
+Vector method
+int main(){
    vector<int> count1(26),count2(26);
    //To space just use count1
    string s1="apple";
@@ -38,4 +69,4 @@ int main(){
    cout<<"Its an enagram";
    }
    
-}
+}*/
