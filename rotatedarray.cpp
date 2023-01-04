835705c73214c57698fc36b818f3f6a13d788333
yashkumar123 <jainys_3@gmail.com>
2023-01-04 05:42:38 +0000

    added rotated array definition

---
+++ rotatedarray.cpp
@@ -0,0 +1,7 @@
+/*In a rotated array an element is taken and brought to the front
+For example suppose array is 
+1,2,3,4,5
+after one rotation it becomes
+5,1,2,3,4
+after second rotation it becomes 
+4,5,1,2,3 and so on*/
