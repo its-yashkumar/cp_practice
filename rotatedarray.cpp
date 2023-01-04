34c047a50ba1a0e693716ce471ee1557b8f71421
yashkumar123 <jainys_3@gmail.com>
2023-01-04 06:01:42 +0000

    added more explanation about problem statement

--- rotatedarray.cpp
+++ rotatedarray.cpp
@@ -1,7 +1,19 @@
-/*In a rotated array an element is taken and brought to the front
+/*We will see how to apply binary search in rotated array
+In a rotated array an element is taken and brought to the front
 For example suppose array is 
 1,2,3,4,5
 after one rotation it becomes
 5,1,2,3,4
 after second rotation it becomes 
 4,5,1,2,3 and so on*/
+/*Example array for given question
+5,6,7,8,9,1,2,3,4 : it is rotated 5 times
+target=6
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    /*First find the mid index and element then find in which region we are either region 1 or region 2
+    First element in region 1 is greater than all elements in region 2*/
+    
+}
