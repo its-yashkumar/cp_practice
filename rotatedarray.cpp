48a7e518bc68d9e517a71a2062b4329521d60ad7
Yashkumar jain <jainy6214@gmail.com>
2023-01-23 19:10:22 +0530

    initialize the variables

--- rotatedarray.cpp
+++ rotatedarray.cpp
@@ -16,5 +16,8 @@ int main(){
     /*First find the mid index and element then find in which region we are either region 1 or region 2
     First element in region 1 is greater than all elements in region 2*/
     vector<int> v={5,6,7,8,9,10,1,2,3,4};
+    int s=0,e=v.size()-1;
+    int target=0,flag=1;
     
+
 }
